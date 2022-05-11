af2070cc44a929255039f5ddf27243d4efa2a547
spitzer <simonausbw@gmail.com>
2022-05-11 13:51:49 +0200

    Erster richtiger Commit

---
+++ PluginTest/src/labyrinth/ausfuehren.java
@@ -0,0 +1,8 @@
+package labyrinth;
+
+public class ausfuehren {
+    public static void main(String[] args) {
+        Maze maze = new Maze(21, 21);//Dieses Mal werde ich ein 21x21 Labyrinth als Versuch machen.
+        maze.show();
+    }
+}
