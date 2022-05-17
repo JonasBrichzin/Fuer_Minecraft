1fcfc83e7926f99136a30d5a8b556500fddb7660
Mario Koepcke <wi21160@lehre.dhbw-stuttgart.de>
2022-05-17 18:30:59 +0200

    doppelte Ordner löschen

--- PluginTest/src/labyrinth/ausfuehren.java
+++
@@ -1,8 +0,0 @@
-package labyrinth;
-
-public class ausfuehren {
-    public static void main(String[] args) {
-        Maze maze = new Maze(21, 21);//Dieses Mal werde ich ein 21x21 Labyrinth als Versuch machen.
-        maze.show();
-    }
-}
